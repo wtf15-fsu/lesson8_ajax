80fcb2f7c73c8662d7ef3eadef9e5515c5d376ca
William Fuller <wtf15@my.fsu.edu>
2018-08-03 15:49:19 +0000

    lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=18c3582b556840d5166624f40a34505a',
                 data: query
             })
             
@@ -57,7 +57,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
-                
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -72,7 +72,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
-
+                 $("#release_date").html(json.results[0].release_date);
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -87,9 +87,9 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/   
 
+                 $("#popularity").html(json.results[0].popularity);
   
 
-                
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
                  TASK #9:  
                  1.  Include the jQuery code below that will retrieve the movie language from JSON object
@@ -102,7 +102,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
-
+                 $("#original_language").html(json.results[0].original_language);
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
--- README.md
+++ README.md
@@ -4,19 +4,17 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
-
+August 3rd, 2018
 ### Student Name:  
-(First and Last Name)
+William Fuller
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+Create an application that pulls data from TMDB (https://www.themoviedb.org/) using a user-selected title
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://wtf15-fsu.github.io/lesson8_ajax/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. How to use AJAX to connect to databases and pull requests
+2. Using jquery to modify a DOM with AJAX-retrieved information
+3. How to debug AJAX queries using the browser console
